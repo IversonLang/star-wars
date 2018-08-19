8738433d4aa8b88db294cb04f4003ac75eb17d22
iverosnlang <iversonlang@yeah.net>
2018-08-19 23:38:34 +0800

    test html

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -5,6 +5,9 @@ export default class Register extends React.Component {
         return (
             <div>
                 <h1>Register page</h1>
+                <span>用户名:</span><input  />
+                <br />
+                <span>密&nbsp;&nbsp;&nbsp;&nbsp;码:</span><input  />
             </div>
         )
     }
