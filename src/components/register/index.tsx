6621ef8bfb783489279bc315d8c1c93b72ff9da4
iverosnlang <iversonlang@yeah.net>
2018-08-26 14:56:15 +0800

    modify register file

--- src/components/register/index.tsx
+++ src/components/register/index.tsx
@@ -1,13 +1,49 @@
 import * as React from "react";
+import axios from "axios";
 
 export default class Register extends React.Component {
+    state = {
+        name: '',
+        passwd: ''
+    }
+
+    constructor(props: any) {
+        super(props);
+        this.handleNameOnChage = this.handleNameOnChage.bind(this);
+        this.handlePasswdOnChage = this.handlePasswdOnChage.bind(this);
+    }
+
+    handleNameOnChage(event: any) {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handlePasswdOnChage(event: any) {
+        console.log(event.target.value)
+        this.setState({
+            passwd: event.target.value
+        })
+    }
+
+    handleBtnSubmit() {
+        console.log('test')
+        axios.post('/http://localhost:8080/account/register', {
+            name: this.state.name,
+            passwd: this.state.passwd
+        }).then(function (response) {
+                console.log(response);
+            }).catch(function (error) {
+                console.log(error);
+            });
+    }
+
     render() {
         return (
             <div>
-                <h1>Register page</h1>
-                <span>用户名:</span><input  />
-                <br />
-                <span>密&nbsp;&nbsp;&nbsp;&nbsp;码:</span><input  />
+                <input placeholder='账号' value={this.state.name} onChange={this.handleNameOnChage}></input>
+                <input placeholder='密码' value={this.state.passwd} onChange={this.handlePasswdOnChage}></input>
+                <button onClick={this.handleBtnSubmit}>注册</button>
             </div>
         )
     }
